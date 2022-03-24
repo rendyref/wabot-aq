2e2fec8c02ff5354c0df88c1c4761186a2f82ab1
rendyref <76547336+rendyref@users.noreply.github.com>
2022-03-24 16:13:28 +0700

    Update _antispam.js

--- plugins/_antispam.js
+++ plugins/_antispam.js
@@ -5,7 +5,7 @@ module.exports = {
         if (m.sender in this.spam) {
             this.spam[m.sender].count++
             if (m.messageTimestamp.toNumber() - this.spam[m.sender].lastspam > 10) {
-                if (this.spam[m.sender].count > 10) {
+                if (this.spam[m.sender].count > 1) {
                     //global.db.data.users[m.sender].banned = true
                     conn.sendFile(m.chat, 'https://cdn.discordapp.com/attachments/843656817056808981/956477814897528832/STK-20220324-WA0011.webp', 'STK-20220324-WA0011.webp', '', m)
                 }
